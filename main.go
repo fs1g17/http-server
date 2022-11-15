5a55471987719e7d768bcfdcba341090772d024c
Fedor Selenskiy <47851444+fs1g17@users.noreply.github.com>
2022-11-15 19:26:09 +0000

    Create main.go
    Port google cloud functionality to this mini Go server

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+type InstaFeed struct {
+	Data []struct {
+		ID string `json:"id"`
+	} `json:"data"`
+	Paging struct {
+		Cursors struct {
+			Before string `json:"before"`
+			After  string `json:"after"`
+		} `json:"cursors"`
+		Next string `json:"next"`
+	} `json:"paging"`
+}
+
+type InstaItem struct {
+	MediaURL     string `json:"media_url"`
+	Permalink    string `json:"permalink"`
+	ThumbnailURL string `json:"thumbnail_url"`
+	MediaType    string `json:"media_type"`
+	ID           string `json:"id"`
+}
+
+const accessToken = "IGQVJXcUdSNDdPOTdHamJBSDJodDdUSXlsbmpCNVZAFYlZABcTlUWU1kWWZAUS0p1ZAFJTVmJpc095bjRfZAHpJTW9zMC10NUVTWk9YV3dQUDRqY0ZAIV2ZAsanF3QmtVdUo1X0tKdHFfM0ZALMFJSZAUVxcTNsSwZDZD"
+const userId = "5626681774019951"
+
+var instaUrl = fmt.Sprintf("https://graph.instagram.com/%s/media?access_token=%s", userId, accessToken)
+
+func main() {
+	http.HandleFunc("/", getInstaFeed)
+
+	err := http.ListenAndServe(":9090", nil)
+
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server closed\n")
+	} else if err != nil {
+		fmt.Printf("error starting server: %s\n", err)
+		os.Exit(1)
+	}
+
+}
+
+func getInstaFeed(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET")
+
+	resp, err := http.Get(instaUrl)
+	if err != nil {
+		fmt.Fprintf(w, "error making http request: %s", err)
+		return
+	}
+
+	var feed InstaFeed
+	if err := json.NewDecoder(resp.Body).Decode(&feed); err != nil {
+		fmt.Fprintf(w, "error decoding insta feed body: %s", err)
+		return
+	}
+
+	data := feed.Data
+
+	var instaItems []InstaItem
+
+	for i := 0; i < 9 || i == len(data); i++ {
+		item := data[i]
+		id := item.ID
+
+		mediaUrl := fmt.Sprintf("https://graph.instagram.com/%s?access_token=%s&fields=media_url,permalink,thumbnail_url,media_type", id, accessToken)
+
+		resp, err := http.Get(mediaUrl)
+		if err != nil {
+			fmt.Fprintf(w, "error getting media item  with id: %s, error: %s", id, err)
+			return
+		}
+
+		var instaItem InstaItem
+		if err := json.NewDecoder(resp.Body).Decode(&instaItem); err != nil {
+			fmt.Fprintf(w, "error deconding media item with id: %s, error: %s", id, err)
+			return
+		}
+
+		instaItems = append(instaItems, instaItem)
+	}
+
+	err = json.NewEncoder(w).Encode(instaItems)
+	if err != nil {
+		fmt.Fprintf(w, "oops %s", err)
+		return
+	}
+
+}
